0ffccb9fe41c9c353b9883088cd70896998e8a70
amiltonxavier <amilton.jose@etic.co.ao>
2025-02-21 17:45:34 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { ArrowRight, Copy } from "lucide-react";
+import { Button } from "./components/button";
+import { IconButton } from "./components/IconButton";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="text-amber-500 font-heading">
+        <Button>
+          Enviar
+          <ArrowRight />
+        </Button>
+        <IconButton>
+          <Copy />
+        </IconButton>
+      </div>
     </main>
   );
 }
